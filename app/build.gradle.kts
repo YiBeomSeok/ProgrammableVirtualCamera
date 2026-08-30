fe6035b5744b2c295523f5f02220b6758ff2a71f
YiBeomSeok <vv99911@gmail.com>
2026-08-30 18:03:18 +0900

    AGP를 9.3.2로 올리고 내장 Kotlin 지원으로 전환
    AGP 9부터 Kotlin 지원이 내장되어 org.jetbrains.kotlin.android 플러그인을
    적용하면 빌드가 거부된다. 플러그인과 jvmTarget을 지정하던 kotlin 블록을
    제거했다.
    
    카탈로그의 kotlin 버전은 AGP 내장 버전인 2.2.10에 맞췄다. 이제 이 값은
    kotlin-test에만 쓰이는데, 컴파일러와 다른 버전을 가리키면 어긋난다.
    
    Gradle도 9.7.1로 함께 올렸다. 에뮬레이터에서 debug와 release 모두 다시
    확인했고 초기화와 해제 결과는 이전과 같다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -31,12 +30,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
